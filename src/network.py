fix rf calculation

src/network.py
@@ -29,10 +29,10 @@ class Net(nn.Module):
         self.dropout2 = nn.Dropout(0.1)
         self.pool2 = nn.MaxPool2d(
             2, 2
-        )  # input 14x14x16 | Output 7x7x16| RF 17 {r_in:14, r_out:17, j_in:2, j_out:4}
+        )  # input 14x14x16 | Output 7x7x16| RF 16 {r_in:14, r_out:17, j_in:2, j_out:4}
         self.conv4 = nn.Conv2d(
             16, 32, 3, padding=1
-        )  # input 7x7x16 | Output 7x7x32| RF 25 {r_in:17, r_out:25, j_in:4, j_out:4}
+        )  # input 7x7x16 | Output 7x7x32| RF 24 {r_in:17, r_out:25, j_in:4, j_out:4}
         self.bn4 = nn.BatchNorm2d(32)
         self.trasition_1 = nn.Conv2d(32, 8, 1)  # input 7x7x32 | Output 7x7x8| RF
         self.fc1 = nn.Linear(8 * 7 * 7, 10)